14bcccb45e517d54431c27147ddb196852f7c9c7
Nick VIntartas <nv2009123@gmail.com>
2024-10-24 10:46:10 -0500

    Initial Commit

---
+++ COYAMiniAppVintartasTests/COYAMiniAppVintartasTests.swift
@@ -0,0 +1,17 @@
+//
+//  COYAMiniAppVintartasTests.swift
+//  COYAMiniAppVintartasTests
+//
+//  Created by NICHOLAS VINTARTAS on 10/24/24.
+//
+
+import Testing
+@testable import COYAMiniAppVintartas
+
+struct COYAMiniAppVintartasTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
